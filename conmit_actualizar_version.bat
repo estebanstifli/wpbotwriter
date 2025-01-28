e491698ca245b8e55b9735d3e1a607b0738347fa
Esteban <estebandezafra@gmail.com>
2025-01-28 08:40:49 +0100

    Actualizaci├│n a la versi├│n 1.2.6. Cambios reflejados en readme.txt.

---
+++ conmit_actualizar_version.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:: Define la nueva versión
+set VERSION=1.2.6
+
+:: Ruta al archivo readme.txt
+set README_PATH=readme.txt
+
+:: Reemplaza la versión en el archivo readme.txt
+"C:\Program Files\Git\usr\bin\sed.exe" -i "s/^Stable tag: .*/Stable tag: %VERSION%/" %README_PATH%
+
+:: Verifica que la versión ha sido actualizada
+echo Nueva versión establecida en %VERSION% en el archivo readme.txt
+
+:: Añade el cambio al staging de Git
+git add %README_PATH%
+
+:: Crear el commit
+git commit -m "Actualización a la versión %VERSION%. Cambios reflejados en readme.txt."
+
+:: Subir los cambios a la rama principal (master)
+git push Github master
+
+:: Crear y subir el tag
+git tag -a %VERSION% -m "Versión %VERSION%"
+git push Github %VERSION%
+
+pause
